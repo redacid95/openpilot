60d0f888a965232b1817d537ed91371ae4447cf1
redacid95 <jonathanc.olivier@gmail.com>
2021-12-05 13:04:50 -0800

    fix image opacity after onroad merge

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -251,7 +251,7 @@ void OnroadHud::drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, flo
   p.setPen(Qt::NoPen);
   p.setBrush(bg);
   p.drawEllipse(x - radius / 2, y - radius / 2, radius, radius);
-  p.setOpacity(opacity);
+  p.setOpacity(opacity ? 1.0f : 0.15f);
   p.drawPixmap(x - img_size / 2, y - img_size / 2, img);
 }
 
