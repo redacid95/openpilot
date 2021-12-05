ddfee0681555c9011dd7ada31c33b7ce43ba4e26
redacid95 <jonathanc.olivier@gmail.com>
2021-12-05 10:56:11 -0800

    Merge pull request #1 from redacid95/brake_ui
    Brake UI
    # Conflicts:
    #	selfdrive/ui/paint.cc

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -100,6 +100,10 @@ typedef struct UIScene {
   mat3 view_from_calib;
   bool world_objects_visible;
 
+  //Brake UI
+  bool brakePressed;
+  bool computerBraking;
+
   cereal::PandaState::PandaType pandaType;
 
   // modelV2
