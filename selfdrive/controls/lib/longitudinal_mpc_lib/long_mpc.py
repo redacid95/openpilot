fe03ac32df0c80571c004ed8deef952572450a3b
redacid95 <jonathanc.olivier@gmail.com>
2022-06-12 10:35:20 -0700

    aggressive long policy

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -50,8 +50,8 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1
 T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
-T_FOLLOW = 1.2
-COMFORT_BRAKE = 2.5
+T_FOLLOW = 1.0
+COMFORT_BRAKE = 2.8
 STOP_DISTANCE = 6.0
 
 def get_stopped_equivalence_factor(v_lead):
