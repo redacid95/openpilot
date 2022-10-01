7c256f4ff270b057a4cb0559cd0c8728f7356111
redacid95 <jonathanc.olivier@gmail.com>
2022-10-01 12:00:23 -0700

    Tuning

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -52,9 +52,9 @@ T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 MAX_ACCEL = 2.0
-T_FOLLOW = 1.15
-COMFORT_BRAKE = 2.42
-STOP_DISTANCE = 5.8
+T_FOLLOW = 1.2
+COMFORT_BRAKE = 2.52
+STOP_DISTANCE = 6.0
 
 def get_stopped_equivalence_factor(v_lead):
   return (v_lead**2) / (2 * COMFORT_BRAKE)
